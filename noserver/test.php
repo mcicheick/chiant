5201f56cfe73a6a10c9f9b1ad787a01712d738ed
amblaf <s@s.fr>
2016-06-24 21:06:19 +0200

    ajout d'un bouton Reset dans les formulaires de test

--- noserver/test.php
+++ noserver/test.php
@@ -10,15 +10,50 @@ if (ENV != 'LOCAL')
 //die ('coucou');
 
 
+?>
+
+<script>
+// js function to reset a form
+function resetForm(form) {
+    // clearing inputs
+    var inputs = form.getElementsByTagName('input');
+    for (var i = 0; i<inputs.length; i++) {
+        switch (inputs[i].type) {
+            // case 'hidden':
+            case 'text':
+                inputs[i].value = '';
+                break;
+            case 'radio':
+            case 'checkbox':
+                inputs[i].checked = false;   
+        }
+    }
+
+    // clearing selects
+    var selects = form.getElementsByTagName('select');
+    for (var i = 0; i<selects.length; i++)
+        selects[i].selectedIndex = 0;
+
+    // clearing textarea
+    var text= form.getElementsByTagName('textarea');
+    for (var i = 0; i<text.length; i++)
+        text[i].innerHTML= '';
+
+    return false;
+}
+</script>
+<?php
+
 function print_route($req, $params, $file) {
     echo '<form action="../entree_post.php" method="post"  enctype="multipart/form-data"><br/>';
 echo '<input type="hidden" name="requete" value="'.$req.'" />';
-    $str = 'requete: <span id="form_'.$req.'">'.$req."</span>\n<br/>paramètres :<br/> ";
+    $str = 'requete: <span id="form_'.$req.'">'.$req."</span>\n<br/>paramètres :<br/> <p/>";
 if ($file)
-   echo '<input type="file" name="'.$file.'" /><br/>';
+   $str .= '<input type="file" name="'.$file.'" /><br/>';
     foreach ($params as $key => $param) 
 	$str .= "\t".$key. ':  <input type="text" name="'.$key.'" value="'.$param.'" />\n<br/>';
    
+    $str .= '<input type="reset" value="Reset" onclick="return resetForm(this.form);">';
     $str .= '<input type="submit" value="submit" /></form><p/>';
 	echo $str;
     return ;
