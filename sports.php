59ab52adde4f43bfb7c9d93f2f1b3f6a87cd0e27
zhimer <zouhair.himer@gmail.com>
2016-07-23 21:31:07 +0200

    Ajout du sport CRICKET
    Car très populaire

--- sports.php
+++ sports.php
@@ -3,13 +3,13 @@
 // CE fichier doit être en accord avec la table SQL REF_SPORTS
 // il faut aussi modifier update_sports_pref en consequence dans requete.php
 
-define('NB_SPORTS', 12);
+define('NB_SPORTS', 13);
 
 
 // Cf structures.sql pour accord (données de la table sql)
 function listSports() {
 	// ID_SPORT => Nom du sport
 	return array(0 => 'football', 1 => 'basketball', 2 => 'tennis', 3 => 'course', 4 => 'billard', 5 => 'volley'
-		, 6 => 'frisbie', 7 => 'pingpong', 8 => 'badminton', 9 => 'petanque', 10 => 'baseball', 11 => 'handball');
+		, 6 => 'frisbie', 7 => 'pingpong', 8 => 'badminton', 9 => 'petanque', 10 => 'baseball', 11 => 'handball',12=> 'cricket');
 }
 // test
